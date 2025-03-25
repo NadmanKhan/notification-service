fc96c015c9c703dbe6008d44cc191a1b190f9836
Nadman A. Khan <nadman.khan@optimizely.com>
2025-03-26 03:37:20 +0600

    refactor: extract out route handlers to make them more suitable for unit tests

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,31 +1,34 @@
 import type { Notification } from "../services/notification";
-import { validateNotification, sendNotification } from "../services/notification";
+import * as service from "../services/notification";
 import logger from "../utils/logger";
 
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
+
+const validateNotification: RequestHandler = (req, res, next) => {
+    try {
+        service.validateNotification(req.body);
+        next();
+    } catch (error) {
+        logger.error(`💥 ${error.message}`);
+        res.status(400).json({ success: false, error: error.message });
+    }
+};
+
+const sendNotification: RequestHandler = async (req, res) => {
+    try {
+        const notification = req.body as Notification;
+        const result = await service.sendNotification(notification);
+        res.status(200).json({ success: true, result });
+    } catch (error) {
+        logger.error(`💥 ${error.message}`);
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
 
 const router = Router();
-
 router.post("/notification",
-    (req, res, next) => {
-        try {
-            validateNotification(req.body);
-            next();
-        } catch (error) {
-            logger.error(`💥 ${error.message}`);
-            res.status(400).json({ success: false, error: error.message });
-        }
-    },
-    async (req, res) => {
-        try {
-            const notification = req.body as Notification;
-            const result = await sendNotification(notification);
-            res.status(200).json({ success: true, result });
-        } catch (error) {
-            logger.error(`💥 ${error.message}`);
-            res.status(500).json({ success: false, error: error.message });
-        }
-    }
+    validateNotification,
+    sendNotification,
 );
 
 export default router;
